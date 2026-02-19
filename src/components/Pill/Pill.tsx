7dc1e24df9d5808cec8c4207f8f557fb4781bf47
Cristiano Giacomi <giacris86@gmail.com>
2026-02-19 15:21:58 +0100

    feat: added Pill component

---
+++ src/components/Pill/Pill.tsx
@@ -0,0 +1,17 @@
+import { clsx } from "clsx"
+import type { ComponentProps } from "react"
+import "./styles.css"
+
+export type PillProps = ComponentProps<"span"> & {
+	variant?: "default" | "airing"
+}
+
+export function Pill({ className, variant = "default", ...rest }: PillProps) {
+	return (
+		<span
+			className={clsx("pill", className)}
+			data-variant={variant}
+			{...rest}
+		/>
+	)
+}
